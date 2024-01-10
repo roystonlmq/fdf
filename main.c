50c985863c855fc10a6aaf27f347b65d6b53dbfe
Royston Lee <roylee@c2r4s3.42singapore.sg>
2024-01-10 21:16:07 +0800

    remove test code

--- main.c
+++ main.c
@@ -1,37 +1,16 @@
 #include "fdf.h"
 
-// int	main(int argc, char **argv)
-// {
-// 	(void) argv;
-// 	t_prog	*prog;
-
-// 	if (argc != 2)
-// 		exception(1, "Not enough arguments");
-// 	prog = init_prog();
-// 	prog->df = init_df(argv[1]);
-// 	parse_df(prog->df, argv[1]);
-// 	printf("Good");
-// }
-
-
-
-int	main(void)
+int	main(int argc, char **argv)
 {
-	int		fd;
-	// int		i = 0;
-	char	*line;
+	(void) argv;
+	t_prog	*prog;
 
-	fd = open("test.txt", O_RDONLY);
-	
-	if (fd < 0)
-		exception(1, "Failed to open file");
-	line = 0;
-	
-	while (ft_next_line(fd, &line) > 0)
-	{
-		
-		printf("%s\n", line);
-		free(line)
-	}
-
+	if (argc != 2)
+		exception(1, "Not enough arguments");
+	prog = init_prog();
+	prog->df = init_df(argv[1]);
+	parse_df(prog->df, argv[1]);
+	printf("Good");
 }
+
+
