88b592b4aebf362ee8f4029040a09da4d0e6b18d
Royston Lee <roylee@c2r4s3.42singapore.sg>
2024-01-10 21:28:35 +0800

    add splitlen func

--- srcs/next_line.c
+++ srcs/next_line.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 21:06:02 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/10 21:06:25 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/10 21:22:04 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 
 int	ft_next_line(int fd, char **line)
 {
-
 	*line = get_next_line(fd);
 	if (*line == 0 || **line == 0)
 		return (0);
 	return (1);
+}
+
+static int	ft_splitlen(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+		i++;
+	return (i);
 }
