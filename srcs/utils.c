e925afadf694764dab9894e3a278362ba579a526
Royston Lee <roylee@c2r4s3.42singapore.sg>
2024-01-13 14:41:35 +0800

    add abs fn

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 11:24:26 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/13 11:26:25 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/13 13:38:09 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,11 @@ void	ft_free_strarr(char **str)
 	while (str[i])
 		free(str[i++]);
 	free(str);
+}
+
+int		ft_abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	return (a);
 }
