6bbc286d1277496b721a7de1c196979747491378
Royston Lee <roylee@c2r4s3.42singapore.sg>
2024-01-13 14:43:12 +0800

    add ability to draw and put img

--- main.c
+++ main.c
@@ -3,14 +3,16 @@
 int	main(int argc, char **argv)
 {
 	(void) argv;
+	(void) argc;
 	t_prog	*prog;
 
-	if (argc != 2)
-		exception(1, "Not enough arguments");
+	// if (argc != 2)
+	// 	exception(1, "Not enough arguments");
 	prog = init_prog();
 	prog->df = init_df();
-	parse_df(prog->df, argv[1]);
-	printf("Good");
+	parse_df(prog->df, "test_maps/42.fdf");
+	draw(prog->df, prog);
+	mlx_put_image_to_window(prog->mlx_ptr, prog->mlx_win, prog->mlx_img, 0, 0);
+	mlx_loop(prog->mlx_ptr);
+	return (0);
 }
-
-
