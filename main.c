d2bf1f140aca11ab947fb08e76003099244d95e0
Royston Lee <roylee@c2r4s3.42singapore.sg>
2024-01-17 21:50:30 +0800

    idk what changes i made alr

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "fdf.h"
 
+
+
 int	main(int argc, char **argv)
 {
 	(void) argv;
@@ -12,13 +14,14 @@ int	main(int argc, char **argv)
 	prog = init_prog();
 	prog->df = init_df();
 	prog->df->h_view = 10;
-	prog->df->h_move = 50;
+	prog->df->h_move = 5;
 	prog->df->v_move = 0;
 	parse_df(prog->df, "test_maps/100-6.fdf");
 	prog->df->zoom = find_max(prog->df->width / prog->df->height, 5);
 	printf("%f %f\n", prog->df->zoom, prog->df->h_view);
 	// adjust_map(prog->df);
 	draw_loop(prog, prog->df);
+	
 	mlx_put_image_to_window(prog->mlx, prog->win, prog->img, 0, 0);
 	mlx_loop(prog->mlx);
 	return (0);
