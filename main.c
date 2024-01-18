26d7da1db7f775d9bae8093bdee66720de0972b2
Royston Lee <roylee@c2r4s3.42singapore.sg>
2024-01-18 20:15:55 +0800

    clean up unused vars

--- main.c
+++ main.c
@@ -7,16 +7,15 @@ int	main(int argc, char **argv)
 	(void) argv;
 	(void) argc;
 	t_prog	*prog;
-	t_point	p1;
-	t_point	p2;
+
 	// if (argc != 2)
 	// 	exception(1, "Not enough arguments");
 	prog = init_prog();
 	prog->df = init_df();
 	prog->df->h_view = 10;
 	prog->df->h_move = WIN_WIDTH / 2;
-	parse_df(prog->df, "test_maps/plat.fdf");
-	
+	parse_df(prog->df, "test_maps/100-6.fdf");
+
 	prog->df->zoom = find_min(WIN_WIDTH / prog->df->t_map->max_x 
 	/ 2, WIN_HEIGHT / prog->df->t_map->max_y / 2);
 	prog->df->v_move = prog->df->t_map->max_y * prog->df->zoom / 2;
