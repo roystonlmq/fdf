bf221d7469ee630b37ce88a9de6b08659ba08a1c
Royston Lee <roylee@c2r4s3.42singapore.sg>
2024-01-18 20:16:04 +0800

    move map_init

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 20:43:30 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/17 22:15:14 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/18 20:13:49 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,18 @@ t_prog	*init_prog(void)
 		exception(1, "Failed to get data address");
 	app->df = NULL;
 	return (app);
+}
+
+void	map_init(t_df *df)
+{
+	df->t_map = (t_map *)malloc(sizeof(t_map));
+	if (!df->t_map)
+		exception(1, "Failed to allocate memory for map");
+	df->t_map->coord = NULL;
+	df->t_map->max_x = 0;
+	df->t_map->max_y = 0;
+	df->t_map->min_x = 0;
+	df->t_map->min_y = 0;
 }
 
 t_df	*init_df(void)
