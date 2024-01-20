0ea608b34811e561c69078ffc155659dde4decce
Royston Lee <roylee@c2r4s3.42singapore.sg>
2024-01-20 16:50:30 +0800

    add key hooks and destroynotify

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 16:03:39 by roylee            #+#    #+#             */
+/*   Updated: 2024/01/20 16:49:43 by roylee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
-
-
 
 int	main(int argc, char **argv)
 {
@@ -10,12 +20,12 @@ int	main(int argc, char **argv)
 
 	printf("All available maps: \n");
 	system("cd test_maps && ls");
-	if (argc != 2)
-		exception(1, "Not enough arguments");
+	// if (argc != 2)
+	// 	exception(1, "Not enough arguments");
 
 	prog = init_prog();
 	prog->df = init_df();
-	parse_df(prog->df, argv[1]);
+	parse_df(prog->df, "test_maps/42.fdf");
 	// prog->df->zoom = find_min(WIN_WIDTH / (prog->df->t_map->max_x / 2),
 	// (WIN_HEIGHT / prog->df->t_map->max_y / 2)); // copied to parser, so it'll be done after finding min_x, etc.
 	//prog->df->zoom = 2; // comment out if not using normal fill map
@@ -33,9 +43,10 @@ int	main(int argc, char **argv)
 	double miny = prog->df->t_map->min_y;
 	double maxx = prog->df->t_map->max_x;
 	double maxy = prog->df->t_map->max_y;
-	// printf("%f %f\n", prog->df->zoom, prog->df->h_view);
 	prog->win = mlx_new_window(prog->mlx, (maxx - minx), (maxy - miny), TITLE);
 	mlx_put_image_to_window(prog->mlx, prog->win, prog->img, 0, 0);
+	mlx_key_hook(prog->win, &key_hook, prog);
+	mlx_hook(prog->win, DestroyNotify, StructureNotifyMask, &ft_close_win, prog);
 	mlx_loop(prog->mlx);
 	return (0);
 }
