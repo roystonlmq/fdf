e83721067612ce36b837507c54770dbb5ac57bd3
roylee <roylee@iamagod.sg>
2024-01-20 18:11:12 +0800

    norm ok

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 19:26:31 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/10 20:53:56 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/20 18:09:50 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
